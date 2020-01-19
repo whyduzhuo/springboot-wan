691c463bf7d60c9599a9384924b5329eb0bdb8a2
万宏远 <1434495271@qq.com>
2020-01-19 18:01:40 +0800

    文件上传

---
+++ wan-common/src/main/java/com/duzhuo/common/config/ResourcesConfig.java
@@ -0,0 +1,30 @@
+package com.duzhuo.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * 通用配置
+ * 
+ * @author ruoyi
+ */
+@Configuration
+public class ResourcesConfig implements WebMvcConfigurer {
+
+    @Resource
+    private ProFileConfig proFileConfig;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /** 本地文件上传路径 */
+        registry.addResourceHandler(proFileConfig.getFileVirtualPath()+"/**").addResourceLocations("file:" + proFileConfig.getFilePath() + "/");
+
+        /** swagger配置 */
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
